09f5840da40e54d0551095236ef23bfee6b2fbd9
R-GURURAJ <guru1guru0@gmail.com>
2024-02-06 14:30:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+/** @format */
+
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected", socket.id);
+
+  socket.on("join_room",(data)=>{
+     if (socket.room) {
+       socket.leave(socket.room);
+     }
+    socket.join(data);
+    socket.room = data;
+  })
+  socket.on("SendMsg", (data) => {
+    console.log(data);
+    socket.to(socket.room).emit("recived_msg", data.msg, socket.id);
+  });
+
+});
+
+server.listen(3001, () => {
+  console.log("server is on 3001");
+});
